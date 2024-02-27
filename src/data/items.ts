cfbff4949877aed88aef074fabb8c528bd94e37b
gonzalo <gon.mb3@gmail.com>
2024-02-27 11:09:04 -0300

    images update

--- src/data/items.ts
+++ src/data/items.ts
@@ -1,28 +1,33 @@
+import book from "../../public/imgs/book.jpg"
+import computer from "../../public/imgs/computer.jpg"
+import banana from "../../public/imgs/banana.jpg"
+import cel from "../../public/imgs/cel.jpg"
+
 
 const storeItems =[
     {
         "id": 1,
         "name":"Libro",
         "price":10.99,
-        "url": "/public/imgs/book.jpg"
+        "url": book
     },
     {
         "id": 2,
         "name":"Computadora",
         "price":109.99,
-        "url": "/public/imgs/computer.jpg"
+        "url": computer
     },
     {
         "id": 3,
         "name":"Banana",
         "price":2.50,
-        "url": "/public/imgs/banana.jpg"
+        "url": banana
     },
     {
         "id": 4,
         "name":"Celular",
         "price":70.50,
-        "url":"/public/imgs/cel.jpg"
+        "url":cel
     }
 ]
 
